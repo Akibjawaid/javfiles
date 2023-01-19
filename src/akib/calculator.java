a868fe749cf6bb8fb2a901f374e5af5d7d0172b0
Akibjawaid <aaquibjawaid121@gmail.com>
2023-01-19 17:12:14 +0530

    java files update

--- src/akib/calculator.java
+++ src/akib/calculator.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while(true){
+        while(true) {
             System.out.println("Calculator ");
-            System.out.println("enter x to exit or continue the operation");
-        System.out.println("enter first number");
-        int b = in.nextInt();
-        System.out.println("enter the second number");
-        int c = in.nextInt();
-
-        System.out.println("enter the operation you want to execute");
+            System.out.println("enter 'X' to exit or 'C' to continue the operation");
+            char f = in.next().charAt(0);
+            if(f=='x'||f=='X'){
+                System.out.println("Exiting program.. Bbyee!!");
+                break;
+            }
+            else if (f=='c'||f=='C'){
+                System.out.println("enter first number");
+            int b = in.nextInt();
+            System.out.println("enter the second number");
+            int c = in.nextInt();
+            System.out.println("enter the operation you want to execute");
         System.out.println("+" + "-" + "*" + "/");
-        char a = in.next().charAt(0);
+            char a = in.next().charAt(0);
 
             switch (a) {
                 case '+':
@@ -27,15 +32,16 @@ public class calculator {
                     System.out.println("the multiplication is = " + (b * c));
                     break;
                 case '/':
-                    System.out.println("the divison is =" + (b / c));
-                    break;
+                    if(a!=0) {
+                        System.out.println("the divison is = " + (b / c) +  "the remainder is = " + (b % c));
+                        break;
+                    }
+                    else {
+                        System.out.println("cannot divide by zero");
+                    }
                 default:
                     System.out.println("invalid input");
             }
-            if(a=='x'|| a=='X'){
-                System.out.println("exiting program");
-                break;
-
             }
             }
         }
